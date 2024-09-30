fddf87ccde31ea69a06c8190588fd139e971724e
mukundmadhav1998 <93767779+mukundmadhav1998@users.noreply.github.com>
2024-09-30 10:42:10 +0000

    container config done

---
+++ 07_Array/array_01.js
@@ -0,0 +1,26 @@
+// ARRAY:-
+// Array is an object in java script which is store multiple type data
+// Array is a non-primitive type data
+
+const myArr = [0,1,2,3,4,5,]
+console.log(myArr[2]);
+
+const myHero = ["Shaktiman", "NagRaj", "IronMan", "Thor"]
+console.log(myHero[0]);
+
+// Array size is dynamic, we can store no of elements and js engnie handle it's automatically 
+
+const myArr2 = new Array(0,1,2,3,4,5,6,7);
+console.log(myArr2);
+const newArr = myArr2;
+console.log(`New Array ${newArr}`);
+
+// Array is reusable
+// Array is a block of memory
+
+
+// Array elements should be separated by comma(,)
+
+// If you try access the index greater then the array length. we will get undefind
+
+// Array Index = length - 1
