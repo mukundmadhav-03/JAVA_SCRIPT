2e8358a49760832686dd43a7f51b48154142629b
mukundmadhav1998 <93767779+mukundmadhav1998@users.noreply.github.com>
2024-10-01 09:45:53 +0000

    container config

---
+++ 07_Array/array_methods_02.js
@@ -0,0 +1,92 @@
+// Array Methods:-
+
+
+
+const myArr = [0,1,2,3,4,5];
+// console.log(myArr);
+
+// push() methods:-
+
+// console.log(myArr.push(6));
+// console.log(myArr);
+myArr.push(7);
+// it is use to insert an element at last of the index
+
+// console.log(myArr);
+myArr.push(8);
+// console.log(myArr);
+// It will return the length of array;
+// ================================================================================================================================
+
+// pop() methods:-
+// It is use to  delete an element from last index 
+
+const myArr2 = [0,1,2,3,4,5,6,7,8,9,10];
+// console.log(myArr2);
+
+myArr2.pop()
+// console.log(myArr2);
+
+myArr2.pop()
+// console.log(myArr2);
+
+// console.log(myArr2.pop());
+
+// console.log(myArr2.pop());
+
+myArr2.pop();
+// console.log(myArr2);
+// It will return the deleted element
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++================================+++++++++++++++++++++++++++++++++++++++++++++
+
+
+// unshift() method:-
+
+let myHeros = ["Ironman", "Thor", "Hulk", "Loki", "Dedpool"]
+// console.log(myHeros);
+
+myHeros.unshift("Black Widow")
+// console.log(myHeros);
+// It is use to insert an element at first index of array
+
+// console.log(myHeros.unshift("Dr.Strange"));
+// console.log(myHeros.unshift("Dr.Jaykaal"));
+
+// const Array = ['Black Widow', 'Ironman', 'Thor', 'Hulk', 'Loki', 'Dedpool','Black Widow', 'Ironman', 'Thor', 'Hulk', 'Loki', 'Dedpool']
+
+// console.log(Array);
+// console.log(Array.unshift("Tamraj Kilwish"));
+// Array.unshift("Mukund Madhav")
+// console.log(Array);
+// It will return Array length
+// =====================================================================================================================================
+
+
+// Shift() method:-
+
+const num = ["abc", "bcd", 'xyz', 'pqr', 'mnp'];
+console.log(num);
+console.log(num.shift());
+
+// It is use to delete an element Starting Index of Array
+
+num.shift()
+console.log(num);
+
+// It will return deleted element
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
