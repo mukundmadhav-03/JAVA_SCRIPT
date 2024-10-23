4f5ffd7d0615b0ba7371744d0e15719d8527187f
mukundmadhav1998 <93767779+mukundmadhav1998@users.noreply.github.com>
2024-10-23 07:29:30 +0000

    function

---
+++ 09_Function/function_01.js
@@ -0,0 +1,53 @@
+function myName(){
+    console.log("M");
+    console.log("U");
+    console.log("K");
+    console.log("U");
+    console.log("N");
+    console.log("D");
+}
+// myName();
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);
+    
+// }
+// addTwoNumbers()  // Nan
+// addTwoNumbers(5,4)  // o/p = 9
+// addTwoNumbers(5,"4")   // o/p = 54
+// addTwoNumbers(4,"a")   // o/p = 4a
+// addTwoNumbers(4+null)   // o/p = 4
+
+
+// function addThreeNumbers(number1,number2,number3){
+    // console.log(number1 + number2 + number3);
+
+    // let result = number1 + number2 + number3
+    // return result;
+
+    // return number1 + number2 + number3;
+
+// }
+// const result = addThreeNumbers(5 , 5 , 5);
+// console.log("result= ", result);
+
+
+
+function userLoginMassage(userName = "Jarvis"){
+    // if(userName === undefined){
+    //       console.log("Please enter a user name");
+    //       return;
+    // }
+
+    if(!userName){
+        console.log("Please enter a user name");
+        return;
+    }
+
+    return `${userName} is login`;
+
+}
+console.log(userLoginMassage("Mukund Madhav"));    // Mukund Madhav is login
+// console.log(userLoginMassage(""));             // is login
+// console.log(userLoginMassage());         // undefined
+
